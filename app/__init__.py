22c113bdd158d29464be617493fcfba9e452ee75
Lint Action <lint-action@samuelmeuli.com>
2021-11-01 04:57:18 +0000

    Fix code style issues with Black

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,7 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
-limiter = Limiter(
-    app,
-    key_func=get_remote_address,
-    default_limits=["200 per minute"]
-)
+limiter = Limiter(app, key_func=get_remote_address, default_limits=["200 per minute"])
 
 # Landing page.
 @app.route("/")
