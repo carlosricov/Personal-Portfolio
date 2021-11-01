9aae426ca35b6d2916fc154bd389bbfca6e34397
carlosricov <carlos.ricoveri.cpe@gmail.com>
2021-11-01 00:56:57 -0400

    Adds rate limiter to flask app

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, url_for, Response
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=["200 per minute"]
+)
 
 # Landing page.
 @app.route("/")
